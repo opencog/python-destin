09e75fed7d5bb61581211b5a54dd37f3dd2c6e2c
Tejas Khot <tjskhot@gmail.com>
2015-05-25 19:13:55 +0530

    added general conv layer

--- general_destin/conv_layer.py
+++ general_destin/conv_layer.py
@@ -3,17 +3,27 @@
 @contact: tjskhot@gmail.com
 
 @note: An implementation of a general Convolutional Neural Network Layer
+
+TODO:
+Add cuda-convnet wrappers from pylearn2 for:
+ProbMaxPool, StochasticMaxPool, WeightedMaxPool, CrossMapNorm
+preferably in a separate pooling file
 """
 import numpy as np
 import theano
 import theano.tensor as T
 from theano.tensor.signal import downsample
 from theano.tensor.nnet import conv
+from pylearn2.sandbox.cuda_convnet.filter_acts import FilterActs
+from theano.sandbox.cuda.basic_ops import gpu_contiguous
+from pylearn2.sandbox.cuda_convnet.pool import MaxPool
 
 import general_destin.nnfuns as nnf 
 
+
 class Layer(object):
 	"""
+	This class implements a general layer of ConvNet 
 	"""
 	def __init__(self,
 				 rng,
@@ -123,24 +133,83 @@ class Layer(object):
 		                  pool_size=(2,2),
 		                  border_mode="valid"):
 		  
-		  """
-		  Convolution operation in this version is not as powerful as using dnn_conv
-		  """
-		  
-		  conv_out=conv.conv2d(input=feature_maps,
-		                       filters=filters, 
-		                       image_shape=feature_shape,
-		                       filter_shape=filter_shape,
-		                       border_mode=border_mode,
-		                       subsample=subsample)
-		  
-		  if pool==True:
-		    pooled_out=downsample.max_pool_2d(input=conv_out,
-		                                      ds=pool_size)
-		  else:
-		    pooled_out=conv_out
-		    
-		  return pooled_out, pooled_out+bias.dimshuffle("x", 0, "x", "x")
+		  	"""
+		 	Convolve input feature maps with filters
+
+		 	@param feature_maps: symbolic tensor of shape feature_shape (theano.tensor.dtensor4)
+		 	@param feature_shape: (batch size, number of input feature maps,
+		 	                       image height, image width) (tuple or list of length 4)
+			@param filters: filters generated within bounds
+		 	@param filter_shape: (number of filters, number of input feature maps,
+		 	                      filter height, filter width) (tuple or list of length 4)
+			@param bias: a 1D tensor -- one bias per output feature map
+			@param subsample: factor by which to subsample the output (tuple of len 2)
+		 	@param pool: Indicates if there is a max-pooling process in the layer. (bool)
+		 	@param pool_size: the pooling factor (#rows, #cols) (tuple or list of length 2)
+		 	@param border_mode: convolution mode
+		 	                    "valid" for valid convolution
+		 	                    "full" for full convolution (string)  
+
+		 	--------------------------------------------------------------------
+		 	Limitations of using FilterActs compared to conv2d:
+
+		 	> Number of channels <= 3; If you want to compute the gradient, it should be divisible by 4.
+		 	> Filters must be square.
+		 	> Number of filters must be a multiple of 16
+		 	> All minibatch sizes are supported, but the best performance is achieved when the minibatch size 
+		 	is a multiple of 128.
+		 	> Works only on the GPU
+
+
+		  	"""
+			if self.check_gpu():
+				input=feature_maps
+				input_shuffled = input.dimshuffle(1, 2, 3, 0) # bc01 to c01b
+				filters_shuffled = filters.dimshuffle(1, 2, 3, 0) # bc01 to c01b
+				## Use zero padding with (filter_size - 1) border i.e. full convolution
+				if border_mode=="full":
+					padding=filter_shape[0]-1
+				else:
+					padding=0
+				conv_out = FilterActs(stride=1, partial_sum=1, pad=padding)
+				contiguous_input = gpu_contiguous(input_shuffled)
+				contiguous_filters = gpu_contiguous(filters_shuffled)
+				conv_out_shuffled = conv_out(contiguous_input, contiguous_filters)
+				if pool==True:
+					pool_op = MaxPool(ds=poolsize[0], stride=poolsize[0])
+					pooled_out_shuffled = pool_op(conv_out_shuffled)
+					pooled_out = pooled_out_shuffled.dimshuffle(3, 0, 1, 2) # c01b to bc01
+				else:
+					pooled_out=conv_out
+
+			else:
+				conv_out=conv.conv2d(input=feature_maps,
+				 					 filters=filters, 
+									 image_shape=feature_shape,
+									 filter_shape=filter_shape,
+									 border_mode=border_mode,
+									 subsample=subsample)
+
+			if pool==True:
+				pooled_out=downsample.max_pool_2d(input=conv_out,
+				              					  ds=pool_size)
+			else:
+				pooled_out=conv_out
+
+			return pooled_out, pooled_out+bias.dimshuffle("x", 0, "x", "x")
+
+		def check_gpu(self):
+			"""
+			Checking if GPU is available
+			"""
+			vlen = 10 * 30 * 768  # 10 x #cores x # threads per core
+
+			rng = numpy.random.RandomState(22)
+			x = shared(numpy.asarray(rng.rand(vlen), config.floatX))
+			f = function([], T.exp(x))
+			if numpy.any([isinstance(x.op, T.Elemwise) for x in f.maker.fgraph.toposort()]):
+			    return False
+			return True
 		
 		def get_activation(self,
 		                   pooled_out):
