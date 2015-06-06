a2ff18a594a12f760bd2c130c421aa21b8ca1674
Tejas Khot <tjskhot@gmail.com>
2015-06-07 00:12:11 +0530

    changed indent size to tab=4

--- destin/nnet/nnfuns.py
+++ destin/nnet/nnfuns.py
@@ -8,37 +8,37 @@
 import theano.tensor as T
 
 def tanh(x):
-  """
-  Hyperbolic tangent nonlinearity
-  """
-  return T.tanh(x);
+    """
+    Hyperbolic tangent nonlinearity
+    """
+    return T.tanh(x);
 
 def sigmoid(x):
-  """
-  Standard sigmoid nonlinearity
-  """
-  return T.nnet.sigmoid(x);
+    """
+    Standard sigmoid nonlinearity
+    """
+    return T.nnet.sigmoid(x);
 
 def softplus(x):
-  """
-  Softplus nonlinearity
-  """
-  return T.nnet.softplus(x);
+    """
+    Softplus nonlinearity
+    """
+    return T.nnet.softplus(x);
 
 def relu(x):
-  """
-  Rectified linear unit
-  """
-  return x*(x>1e-13);
+    """
+    Rectified linear unit
+    """
+    return x*(x>1e-13);
 
 def softmax(x):
-  """
-  Softmax function
-  """
-  return T.nnet.softmax(x);
+    """
+    Softmax function
+    """
+    return T.nnet.softmax(x);
 
 def linear(x):
-  """
-  Linear function
-  """
-  return x;
+    """
+    Linear function
+    """
+    return x;
